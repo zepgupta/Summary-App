36b8a9c11f118337cf9330dc0c527a5cf61c3877
Tyler Martinez <slightlytyler@gmail.com>
2016-02-16 16:22:15 -0600

    Fix store/index lint

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,29 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from 'reducers'
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from 'reducers';
 
-export default function configureStore (initialState = {}) {
+export default function configureStore(initialState = {}) {
   // Compose final middleware and use devtools in debug environment
-  let middleware = applyMiddleware(thunk)
+  let middleware = applyMiddleware(thunk);
   if (__DEBUG__) {
     const devTools = window.devToolsExtension
       ? window.devToolsExtension()
       : require('containers/DevTools').default.instrument()
-    middleware = compose(middleware, devTools)
+    ;
+
+    middleware = compose(middleware, devTools);
   }
 
   // Create final store and subscribe router in debug env ie. for devtools
-  const store = middleware(createStore)(rootReducer, initialState)
+  const store = middleware(createStore)(rootReducer, initialState);
 
   if (module.hot) {
     module.hot.accept('reducers', () => {
-      const nextRootReducer = require('reducers').default
+      const nextRootReducer = require('reducers').default;
 
-      store.replaceReducer(nextRootReducer)
-    })
+      store.replaceReducer(nextRootReducer);
+    });
   }
-  return store
+
+  return store;
 }
