4d2d7c0008998475ad9060661d667a2a9095dc78
Tyler Martinez <slightlytyler@gmail.com>
2016-02-16 18:10:20 -0600

    Outline next step

--- src/pods/summaries/actions.js
+++ src/pods/summaries/actions.js
@@ -6,9 +6,27 @@ const {
 } = actionTypes;
 
 export function createSummary(article) {
-  return {
-    type: CREATE_SUMMARY,
-    id: shortid.generate(),
-    article,
-  };
+  return (dispatch, getState) => {
+    const id = shortid.generate();
+
+    // Create record for summary using article
+    dispatch({
+      type: CREATE_SUMMARY,
+      id,
+      article,
+    });
+    // Fetch summary from api
+    //
+    // If error alert user
+    //
+    // If successfull proceeed
+    //
+    // Update record with summary (delete article text?)
+    // dispatch({
+    //   type: UPDATE_SUMMARY,
+    //   id,
+    //   summary,
+    // });
+    // Transition to summary show screen
+  }
 }
